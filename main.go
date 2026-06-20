8b9717cddc880e6cd680a5ddd3c99ae144d5c7f1
Usman Qamar <usmanq03@gmail.com>
2026-06-20 11:12:55 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href="mailto:jon@calhoun.io">jon@calhoun.io</a>.`)
+}
+
+func main() {
+    http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/contact", contactHandler)
+    fmt.Println("Starting the server on :3000...")
+    http.ListenAndServe(":3000", nil)
+}

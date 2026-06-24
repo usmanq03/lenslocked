79f54a3b40301404730c5d28a8ab98fdf0825380
Usman Qamar <usmanq03@gmail.com>
2026-06-24 17:37:30 -0400

    updating pathhandler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/usmanq03/lenslocked
+
+go 1.25.0
--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href="mailto:jon@calhoun.io">jon@calhoun.io</a>.`)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href="mailto:me@uqamar.co">me@uqamar.co</a>`)
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.WriteHeader(450)
+		fmt.Fprint(w, "<p>This page does not exist</p>")
+	}
 }
 
 func main() {
-    http.HandleFunc("/", homeHandler)
-    http.HandleFunc("/contact", contactHandler)
-    fmt.Println("Starting the server on :3000...")
-    http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/contact", contactHandler)
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
 }
